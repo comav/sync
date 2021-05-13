55d2ccde652e6e654fbde903b8d8f65d7c7b7c40
comav <tsyomav@gmail.com>
2021-05-13 20:43:16 +0300

    File reading implemented

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import ReactDOM from 'react-dom';
+import { useDropzone } from 'react-dropzone';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+//const app = window.require('electron');
+
+function MyDropzone() {
+  let files = [];
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader()
+
+      let fileName = file.name;
+      let fileType = file.type;
+      console.log(fileType);
+
+      reader.onabort = () => console.log('file reading was aborted')
+      reader.onerror = () => console.log('file reading has failed')
+      reader.onload = () => {
+        const binaryStr = reader.result;
+
+        files.push({
+          "fileName": fileName,
+          "fileType": fileType,
+          "fileContents": binaryStr
+        })
+
+        console.log(files)
+      }
+      reader.readAsArrayBuffer(file)
+    })
+    
+  }, [])
+  //send files to the server
+
+
+
+  const {getRootProps, getInputProps} = useDropzone({onDrop});
+  let divProps = getRootProps()
+  
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      <img src="upload.png" alt=""></img>
+      <p>Drag 'n' drop some files here, or click to select files</p>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  React.createElement(MyDropzone, {}, null),
+  document.querySelector('#root')
+)
